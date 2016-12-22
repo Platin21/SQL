ea4c214d0c8b2b4ab5ba04c9d2aa1563d7a7018e
Platin21 <arminmops@icloud.com>
2016-12-22 09:13:14 +0100

    Addet New Numbers

--- MathWithNumbers.sql
+++ MathWithNumbers.sql
@@ -1,6 +1,22 @@
 -- Lesson Things !
 -- Random Number from 10 to 1 
 SELECT TRUNCATE(RAND()*10,0) AS 'Random';
+
+-- Claissic if 
+SELECT IF('','','') FROM `ahPersonal`;
+
+-- Tests if it is null and wehn it is then it will 
+-- be Replaced by the Second param
+SELECT IFNULL(`abdatum`,'NULL?') FROM `ahAuftraege`;
+
+
+SELECT * FROM `ahAuftraege` WHERE IFNULL(`abdatum`);
+
+-- This Retuns True or False wich Depends on the Presence of the abdatum
+SELECT * FROM `ahAuftraege` WHERE NOT ISNULL(`abdatum`);
+
+SELECT CONCAT(IF(`geschlecht`='w','FRAU','MAN'),' ',`pname`) AS 'Anrede' FROM `ahPersonal`;
+
 -- END Lesson
 
 
@@ -11,11 +27,13 @@ SELECT `pname`,`gehalt`, (`gehalt` / 100) * 5 + `gehalt` AS '5% Erhöht' FROM `a
 SELECT `pname`,`gehalt`,`gehalt` + 50 FROM `ahPersonal` WHERE `gehalt` >= 4000;
 
 -- 9.
-SELECT SUM(((`gehalt` / 100) * 10) + `gehalt`) FROM `ahPersonal` WHERE `gehalt` < 3000;
+SELECT SUM(`gehalt`) / COUNT(*) FROM `ahPersonal` WHERE `gehalt` < 3000;
 
+-- 10.
 CREATE TABLE geoFigur
 (
-	nummer INT PRIMARY KEY,
-    
+	nummer INT PRIMARY KEY AUTO_INCREMENT,
+    _type INT,
+    bez
 )
 -- 10.

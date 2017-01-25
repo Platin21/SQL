262be391cd4cd06082bc21497a2a89332438a7cf
Platin21 <arminmops@icloud.com>
2017-01-25 08:58:03 +0100

    Change: Removed some Comments usw.

--- Hamar_SortAndGroup.sql
+++ Hamar_SortAndGroup.sql
@@ -1,8 +1,9 @@
-# //++ Module Icon ++//
+# //++ Sort and Group ++//
  /*
  #MK        Armin Hamar
  #VERS      0.0.1
- #INFOTEXT  Sortieren und Gruppieren UE-SortierenGruppieren.pdf  
+ #INFOTEXT  Sortieren und Gruppieren
+ #Aufgabe   UE-SortierenGruppieren.pdf
  #DATE 		18-01-2017
  */
  
@@ -37,8 +38,7 @@
  # aufsteigend, innerhalb der Auftragsnummern nach Personalnummern absteigend und innerhalb
  # gleicher Personalnummern nach Tätigkeitsarten aufsteigend sortiert angezeigt werden!
  # -------------------------------------------------------------------------------#
- # I don't know if it works but i think it normaly should not work at all because 
- # of the Ordering by Diffrent sort methods ASC and DESC
+ # I don't know can not check for equality `tbnr`
  SELECT 
 	`persnr` ,
      `aufnr` ,
@@ -61,7 +61,7 @@
  FROM 
 	`ahFamilienmitglieder` 
  GROUP BY 
-	`persnr`;
+	`persnr`;						# Groups by `persnr`
  
  # 5. Gib Nachname und Eintrittsdatum jener 4 Mitarbeiter aus, welche die wenigsten Dienstjahre
  # haben!
@@ -87,7 +87,7 @@ GROUP BY
 # 7. Gib Personalnummer und Gehaltssumme pro Mitarbeiter im Jahr 2002 aus!
 # -------------------------------------------------------------------------------#
 # Note Little Error jahr is int(2) and cannot be 2002 because this would be  
-# to larg to fit into the database !   
+# to larg to fit into the database ! And also has
 SELECT 
 	`persnr` , 
     SUM( `mgehalt` ) 
@@ -96,7 +96,7 @@ FROM
 GROUP BY 
 	`persnr` 
 HAVING 
-	`jahr` = 2002;
+	`jahr` = 2002;     # Is not Possible to compare
 
 # 8. Ermittle für jeden Auftrag in Tabelle taetigkeitsposten die Summe der Tätigkeitsstunden!
 # Gib das Ergebnis zusammen mit der Auftragsnummer aus.
@@ -109,7 +109,7 @@ SELECT
 FROM 
 	`ahTaetigkeitsposten`
 GROUP BY 
-	`aufnr`;
+	`aufnr`;				# Groups diffrent values by `aufnr`
 
 # 9. Ermittle für jede Tätigkeitsart in Tabelle taetigkeitsposten die Summe der Tätigkeitsstunden!
 # Es sollen nur die Ergebnisse für die Tätigkeitsarten 3 bis 6 ausgegeben werden.
@@ -126,20 +126,22 @@ FROM
 GROUP BY 
 	`tartnr` 
 HAVING 
-	`tartnr` = 3 OR
+	`tartnr` = 3 OR # Filter if `tartnr` is 3 or 6
     `tartnr` = 6;
 
 # 10. Bilde Mitarbeitergruppen für jeden Anfangsbuchstaben des Nachnamens und zähle die Anzahl der
 # Mitarbeiter in dieser Gruppe!
 # Es sollen nur jene Zeilen ausgegeben werden, deren Anzahl größer als 1 ist.
 # -------------------------------------------------------------------------------#
-# A little bit Odd i think it is easy but i don't know if it works 
-SELECT 
-	SUM(`pname`) AS 'Anzahl'
+# A little bit Odd i think it is easy but i don't know if it works i mean i have tested it
+# but it has some kind'a weird
+SELECT
+	LEFT(`pname`,1) AS 'Name Anfang',
+	COUNT(`pname`) AS 'Anzahl' # Calculate the sum of all pname's
 FROM 
-	`ahPersonal`
+	`ahPersonal` # Table selector
 GROUP BY 
-	LEFT(1,`pname`)
+	LEFT(`pname`,1)
 HAVING
-	SUM(`pname`) > 1;
+	COUNT(LEFT(`pname`,1)) > 1; # Only if pname sum is bigger than one
 
